083de2e4277001b4f7d5253a4659cf336f1f6148
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-13 18:07:15 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,37 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = (props) => {
   const [counter, setCounter] = useState(0);
-  const data = useState(props.slides);
-  
+  const [data] = useState(props.slides);
+
   return (
     <div>
       <h1 data-testid="title">{data[counter].title}</h1>
       <p data-testid="text">{data[counter].text}</p>
-
-      <button data-testid="button-next"
+      <button
+        data-testid="button-next"
         disabled={counter === props.slides.length - 1 ? true : false}
-        onClick={() => setCounter(counter + 1)}> Next </button>
-
-      <button data-testid="button-prev"
+        onClick={() => setCounter(counter + 1)}
+      >
+        Next
+      </button>
+      <button
+        data-testid="button-prev"
         disabled={counter === 0 ? true : false}
-        onClick={() => setCounter(counter - 1)}> Prev </button>
-
-      <button data-testid="button-restart"
+        onClick={() => setCounter(counter - 1)}
+      >
+        Prev
+      </button>
+      <button
+        data-testid="button-restart"
         disabled={counter === 0 ? true : false}
-        onClick={() => setCounter(0)}> Restart </button>
+        onClick={() => setCounter(0)}
+      >
+        Restart
+      </button>
     </div>
   );
 };
-
 
 export default App;
